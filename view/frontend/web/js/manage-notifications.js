dc26d5658ba2843b62b41258e41e8a425e8f6a9d
Marta Siewierska-Stańczak <marta.siewierska-stanczak@creativestyle.pl>
2020-08-06 08:39:14 +0200

    Add jquery widget to manage pwa notifications

--- view/frontend/web/js/manage-notifications.js
+++ view/frontend/web/js/manage-notifications.js
@@ -1,46 +1,46 @@
+
 define([
-    'jquery',
-], function ($) {
-    'use strict';
-
-    function makeRequest(manageUrl, data) {
-        $('body').trigger('processStart');
-
-        $.ajax({
-            url: manageUrl,
-            data: data,
-            dataType: 'json',
-
-            /**
-             * Success callback.
-             */
-            success: function () {
-                //success action
-            },
-
-            /**
-             * Complete callback.
-             */
-            complete: function () {
-                $('body').trigger('processStop');
-            }
-        });
-    }
-
+    'jquery'
+], function ($) { 
+    $.widget('magesuite.pwaNotificationManage', {
+        options: {
+            checkboxSelector: 'input[type="checkbox"]',
+            manageUrl: ''
+        },
         /**
          * Initializes event listener to send ajaz request when checkbox state changes
-         *
-         * @param {Object} config - Optional configuration
-         * @param {HTMLElement} el - DOM element.
          */
-        function init(config, el) {
-            $('.form-pwanotifications-manage input[type="checkbox"]').on('change', function () {
+        _create: function () {
+            var widget = this;
+            this.element.find(this.options.checkboxSelector).on('change', function () {
                 var item = $(this)[0];
                 var data = {[item.name]: item.checked};
-                makeRequest(config.manageUrl, data);
+                widget.makeNotificationChangeRequest(widget.options.manageUrl, data);
+            });
+        },
+        makeNotificationChangeRequest: function(manageUrl, data) {
+            $('body').trigger('processStart');
 
-            });        
-        }
-
-        return init;
+            $.ajax({
+                url: manageUrl,
+                data: data,
+                dataType: 'json',
+    
+                /**
+                 * Success callback.
+                 */
+                success: function () {
+                    //success action
+                },
+    
+                /**
+                 * Complete callback.
+                 */
+                complete: function () {
+                    $('body').trigger('processStop');
+                }
+            });
+        },
+    });
+    return $.magesuite.pwaNotificationManage; 
 });
